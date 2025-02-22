a50d846099f0d5482b327924f16d88e5ee8b13a5
Vincent Bui <vincent.bui657@gmail.com>
2025-02-21 20:32:34 -0500

    Finalize Auth

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,7 +18,7 @@ import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { createAccount } from "@/lib/actions/user.actions";
+import { createAccount, signInUser } from "@/lib/actions/user.actions";
 import OtpModal from "@/components/OTPModal";
 
 
@@ -62,10 +62,13 @@ const AuthForm = ({type}: {type: FormType}) => {
     setErrorMessage("");
 
     try {
-      const user =  await createAccount({
-        fullName: values.fullName || "",
-        email: values.email,
-      });
+      const user =
+        type === "sign-up"
+          ? await createAccount({
+              fullName: values.fullName || "",
+              email: values.email,
+            })
+          : await signInUser({ email: values.email });
   
       setAccountId(user.accountId);
     } catch {
@@ -73,8 +76,6 @@ const AuthForm = ({type}: {type: FormType}) => {
     } finally {
       setIsLoading(false);
     }
-
-   
   };
 
 
