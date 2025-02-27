f29f8b1f2214adeb46842e0b6528ea26b146ce6b
Vincent Bui <vincent.bui657@gmail.com>
2025-02-26 22:01:23 -0500

    Dynamic file routes, file card, check

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
-      bodySizeLimit: "100MB",
+      bodySizeLimit: '100mb',
     },
   },
   images: {
